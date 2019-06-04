b2e17d2c4ec7672cb6280bf54f860f46fe05b519
Va Vong <va058426@gmail.com>
2019-06-04 16:14:42 -0700

    Adds animations

---
+++ NFRS Hanover/Assets/Scripts/ConsoleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ConsoleController : MonoBehaviour
+{
+    private System.Random rnd;
+    [SerializeField] private int seed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rnd = new System.Random(seed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var animator = this.gameObject.GetComponent<Animator>();
+        animator.SetFloat("Random", rnd.Next(0,12));
+    }
+}
---
+++ NFRS Hanover/Assets/Scripts/LaserController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    private System.Random rnd;
+    [SerializeField] private int seed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rnd = new System.Random(seed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var animator = this.gameObject.GetComponent<Animator>();
+        animator.SetFloat("Random", rnd.Next(0,64));
+    }
+}
